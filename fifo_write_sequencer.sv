4b7c0787a594742f5782af098dc9943521d1dd57
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2025-10-08 00:35:13 +0530

    Create fifo_write_sequencer.sv

---
+++ fifo_write_sequencer.sv
@@ -0,0 +1,7 @@
+class fifo_write_sequencer extends uvm_sequencer#(fifo_seq_item);
+        `uvm_component_utils(fifo_write_sequencer)
+
+        function new(string name = "fifo_write_sequencer", uvm_component parent);
+                super.new(name, parent);
+        endfunction
+endclass
